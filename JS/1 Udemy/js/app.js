63592b2e02ef46e09b107d9a5e5948e4b7273517
RV411 <ivan_rodriguez3000@hotmail.com>
2022-12-04 23:02:03 -0600

    carrito

--- JS/1 Udemy/js/app.js
+++ JS/1 Udemy/js/app.js
@@ -2,11 +2,12 @@ const carrito =document.querySelector('#carrito');
 const contenedorCarrito =document.querySelector('#contenedor-carrito tbody');
 const vaciarCarritoBtn =document.querySelector('#vaciar. carrito');
 const listaCursos =document.querySelector('#lista- cursos');
+let articulosCarrito=[];
 
 cargarEventListeners();
 function cargarEventListeners() {
     //agregar curso "Agregar al carrito"
-    listaCursos.addEventListener('click',agregarCurso)
+    listaCursos.addEventListener('click',agregarCurso);
 }
 
 // function
@@ -24,6 +25,46 @@ function leerDatosCurso(curso) {
         imagen:curso.querySelector('img').src,
         titulo:curso.querySelector('h4').textContent,
         precio:curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad:1
     }
+
+    //Agregar al carrito
+    articulosCarrito=[...articulosCarrito,infoCurso];
+
     console.log(infoCurso);
+
+    carritoHTML();
+}
+
+function carritoHTML(){
+    limpiarHTML();
+
+    articulosCarrito.forEach(curso=>{
+        const{imagen,titulo,precio,cantidad,id}=curso;
+        const row=document.createElement('tr');
+        row.innerHTML=`
+        <td>
+            <img src="${imagen}" width="100">
+        </td>
+        <td>${titulo}</td>
+        <td>${precio}</td>
+        <td>${cantidad}</td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${id}">x</a>
+        </td>
+        `;
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+function limpiarHTML() {
+    //* forma lenta    
+    //articulosCarrito.innerHTML='';
+
+    //* forma rapida
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+
 }
