39c30d9c0b2581a792e379649ae789252881a487
RV411 <ivan_rodriguez3000@hotmail.com>
2022-12-19 23:10:08 -0600

    modified:   JS/13 Webpack-ESLint/js/index.js new file:   JS/13 Webpack-ESLint/views/inicio.pug new file:   JS/13 Webpack-ESLint/views/layout/footer.pug new file:   JS/13 Webpack-ESLint/views/layout/header.pug new file:   JS/13 Webpack-ESLint/views/layout/index.pug modified:   JS/13 Webpack-ESLint/views/nosotros.pug

--- JS/13 Webpack-ESLint/js/index.js
+++ JS/13 Webpack-ESLint/js/index.js
@@ -1,8 +1,25 @@
 // const express=requiere('express');
-import { express } from "express";
+import express from "express";
+import router from '../routes/index.js'
+
 const app=express();
 
 const port =process.env.PORT||4000;
+
+//habilitar al PUG
+app.set('view engine','pug');
+
+app.use((req,res,next)=>{
+    const year=new Date();
+    res.locals.actualyear=year.getFullYear();
+    return next();          //? obliga a pasar al siguiente use
+})
+
+//Integrar la carpeta public
+app.use(express.static('public'));
+
+//Agregar router
+app.arguments('/',router);
 
 app.listen(port,()=>{
     console.log(`Servidor .... ${port}`);
